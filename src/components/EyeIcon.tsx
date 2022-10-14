11a9d5ebfd891226ac22d62ca9608e616f3998e1
RafaV12 <rafaelv1926@gmail.com>
2022-10-14 16:32:41 -0400

    Balance history layout, show/hide balance

--- src/components/EyeIcon.tsx
+++ src/components/EyeIcon.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const EyeIcon = () => {
+type EyeIconProps = {
+  showBalance: () => void;
+};
+
+const EyeIcon = ({ showBalance }: EyeIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -13,6 +17,7 @@ const EyeIcon = () => {
       stroke-linecap="round"
       stroke-linejoin="round"
       className="feather feather-eye"
+      onClick={showBalance}
     >
       <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
       <circle cx="12" cy="12" r="3"></circle>
